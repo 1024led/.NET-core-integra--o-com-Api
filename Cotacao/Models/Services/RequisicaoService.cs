b00f37832536f91fc8b25e4b117c64098e3ac224
kaled maciel <kaledmaciel@gmail.com>
2020-10-23 09:47:44 -0300

    Update
    Organização de classes

--- Cotacao/Models/Services/RequisicaoService.cs
+++ Cotacao/Models/Services/RequisicaoService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Threading.Tasks;
 
 namespace Cotacao.Models.Services
@@ -12,6 +13,31 @@ namespace Cotacao.Models.Services
         public string ano { get; set; }
 
         public string moeda { get; set; }
+
+        private string hold1 = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda=%27";
+        private string hold2 = "%27&@dataCotacao=%27";
+        private string hold3 = "%27&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao";
+
+        private string Url{
+            get
+            {
+                string url = hold1 + moeda + hold2 + Mes + "-" + Dia + "-" + ano + hold3;
+                return url;
+            }
+        }
+
+        public void AtualizarUrl()
+        {
+            string url = hold1 + moeda + hold2 + Mes + "-" + Dia + "-" + ano + hold3;
+            return;
+        }
+
+        public string ObterUrl()
+        {
+            return Url;
+        }
+
+
 
     }
 }
